11c51a00b0961deeabcc5e496f61f5ffa9e57f43
Vladimir Fedoseev <vladimir.fedoseev.2004@mail.ru>
2023-09-27 11:11:58 +0300

    Perfect Ellipse

--- sfml.2/03/main.cpp
+++ sfml.2/03/main.cpp
@@ -4,23 +4,27 @@
 
 int main()
 {
-    constexpr int pointCount = 100;
-    constexpr float circleRadius = 200;
+    constexpr int pointCount = 200;
+    const sf::Vector2f ellipseRadius = {200.f, 80.f};
 
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Ellipse");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(
+        sf::VideoMode({800, 600}), "Ellipse",
+        sf::Style::Default, settings);
 
-    sf::ConvexShape shape;
-    shape.setPosition({400, 320});
-    shape.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+    sf::ConvexShape ellipse;
+    ellipse.setPosition({400, 320});
+    ellipse.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
 
-    shape.setPointCount(pointCount);
+    ellipse.setPointCount(pointCount);
     for (int pointNo = 0; pointNo < pointCount; ++pointNo)
     {
         float angle = float(2 * M_PI * pointNo) / float(pointCount);
         sf::Vector2f point = sf::Vector2f{
-            circleRadius * std::sin(angle),
-            circleRadius * std::cos(angle)};
-        shape.setPoint(pointNo, point);
+            ellipseRadius.x * std::sin(angle),
+            ellipseRadius.y * std::cos(angle)};
+        ellipse.setPoint(pointNo, point);
     }
 
     while (window.isOpen())
@@ -35,7 +39,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(ellipse);
         window.display();
     }
 }
